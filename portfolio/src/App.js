f34db10b92075eac2732e7c04bceea578c421d33
aljoker2001 <aljoker2001@hotmail.com>
2020-01-15 20:29:25 -0800

    added splash image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,14 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Splash from './components/Splash';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Splash />
       <About />
       <Portfolio />
       <Contact />
---
+++ portfolio/src/components/Splash.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Splash() {
+    return (
+        <div className='splash d-flex align-items-center'>
+            <h1>Welcome! I'm Alex and I design web apps.</h1>
+        </div>
+    )
+}
+
+export default Splash;
