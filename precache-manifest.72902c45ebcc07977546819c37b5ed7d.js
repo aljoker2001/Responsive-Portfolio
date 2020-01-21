e8a514f8a31ebd647b45436e7dd85c95d312d669
aljoker2001 <aljoker2001@hotmail.com>
2020-01-21 11:07:23 -0800

    Updates

---
+++ precache-manifest.72902c45ebcc07977546819c37b5ed7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4eee155e3ada93a8b16cc1f0fb8dd82",
+    "url": "/Responsive-Portfolio/index.html"
+  },
+  {
+    "revision": "3206696cc1ac1a697b15",
+    "url": "/Responsive-Portfolio/static/css/main.736a90a6.chunk.css"
+  },
+  {
+    "revision": "99736265f272f64c7da7",
+    "url": "/Responsive-Portfolio/static/js/2.b6629ba0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Responsive-Portfolio/static/js/2.b6629ba0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3206696cc1ac1a697b15",
+    "url": "/Responsive-Portfolio/static/js/main.15d1fd34.chunk.js"
+  },
+  {
+    "revision": "efa549926c46f05b8075",
+    "url": "/Responsive-Portfolio/static/js/runtime-main.b4857313.js"
+  }
+]);
